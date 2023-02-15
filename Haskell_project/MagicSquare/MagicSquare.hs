d20e1b7c48cefebf1238d284a23f07f2303a4d99
Kevin Boschetti <65442174+ItzBosk@users.noreply.github.com>
2023-02-15 11:11:25 +0100

    finishhh
    - sistemati print a terminale
    - generato .exe

--- Haskell_project/MagicSquare/MagicSquare.hs
+++ Haskell_project/MagicSquare/MagicSquare.hs
@@ -1,99 +1,69 @@
 import Data.List
 
-type Square = [[Int]]  -- ogni magic square è una lista di liste di Int
+type Square = [[Int]] 
 
--- posso generare tutte le possibili magic square ruotando quella in input e specchiandola
--- ruota di 90 gradi la magic square
-rot90 :: Square -> Square
--- transpose -> righe diventano colonne
--- map reverse -> inverte ogni riga
-rot90 = map reverse . transpose
+printSquare :: Square -> IO ()
+printSquare = putStrLn . unlines . map (unwords . map show)       
 
--- specchia la magic square orizzontalmente
-refl :: Square -> Square
--- transpose riflette la matrice sulla diagonale
-refl = transpose
+chop :: Int -> [Int] -> [[Int]]
+chop _ [] = []  -- se arrivato alla fine return lista vuota
+chop n xs = take n xs:chop n(drop n xs)  -- ricorsione, toglie i primi n elementi dalla lista e prosegue
 
--- stampa la magic square non in linea(default)
-pp :: Square -> IO ()
--- mappo ogni riga, poi all'interno mappo ogni elemento rendendolo una stringa
--- unisco ogni stringa separandole con uno spazio
--- separo le stringhe
-pp = putStrLn . unlines . map (unwords . map show)  
+isMagic :: Square -> Bool
+isMagic s = 
+    (==1) $ -- check se lista di somme = 1
+    length $ 
+    nub $ 
+    concat [map sum s,  -- righe
+            map sum $ transpose s,  -- colonne
+            [sum $ map (uncurry (!!)) $ zip s [0..]],  -- diagonale principale
+            [sum $ map (uncurry (!!)) $ zip (map reverse s) [0..]]]  -- diagonale secondaria
 
--- scelgo di generare tutte le possibili magic square partendo da una effettiva
+allMagicBruteForced :: [Square]
+allMagicBruteForced = filter isMagic $ map (chop 3) $ permutations [1..9]
+
+distance :: Square -> Square -> Int
+distance s1 s2 = sum $ map abs $ zipWith (-) (concat s1) (concat s2)
+
+solve :: Square -> Int
+solve s = minimum $ map (distance s) allMagic
+
 magic :: Square
 magic = [[8, 1, 6],
          [3, 5, 7],
          [4, 9, 2]]
 
--- tutte le possibili magic square 3x3 con numeri da 1 a 9
+rot90 :: Square -> Square
+rot90 = map reverse . transpose
+
+refl :: Square -> Square
+refl = transpose
+
 allMagic :: [Square]
--- filter -> considera solo elementi che rispecchiano la condizione isMagic
--- chop n -> taglia una lista ogni n elementi 
--- permutations -> tutte le possibili permutazioni di una lista
-allMagic = filter isMagic $ map (chop 3) $ permutations [1..9]
-
--- obiettivo del programma: 
-    -- brute force di tutte le possibili magic square derivabili dall'originale
-    -- calcolare la distanza tra l'originale e tutte le 8 possibili derivate
-    -- considerare quella con la minor distanza (somma dei valori assoluti delle distanze tra il valore di un numero nell'originale e nella magic square finale)
-
-distance :: Square -> Square -> Int
--- concat -> rende una lista di liste una lista unica
--- zipWith (-) -> fa la sottrazione elemento per elemento
--- abs -> valore assoluto
--- map -> rimappa la lista usando abs
--- sum -> sommatoria di tutti i valori assoluti delle differenze
-distance s1 s2 = sum $ map abs $ zipWith (-) (concat s1) (concat s2)
-
-solve :: Square -> Int
--- map (distance s) allMagic -> calcola tutte le possibili distanze dalla matrice originale alle possibili magic square
--- minimum -> prende la distanza minore
-solve s = minimum $ map (distance s) allMagic
-
-chop :: Int -> [Int] -> [[Int]]
--- opposto di concat, da una lista di int a una matrice
-chop _ [] = []  -- se arrivato alla fine return lista vuota
-chop n xs = take n xs:chop n(drop n xs)  -- ricorsione, toglie i primi n eleemtni dalla lista e prosegue
-
-isMagic :: Square -> Bool -- true se tutti e quattro i valori calcolati sono uguali
--- transpose -> ogni riga diventa colonna
--- !! -> permette di scegliere la posizione del valore della lista da estrarre
--- zip [0 .. ] -> unisce due liste, con [0..] lista infinta a partire da 0, avrò delle coppie (numero, riga matrice)
--- uncurry -> converte una funzione di modo che accetti delle coppie di elementi
--- nub -> rimuove gli elementi duplicati di una lista
-
--- map sum s -> somma dei numeri delle righe
--- map sum $ transpose s -> somma dei numeri sulle colonne 
--- sum $ map (uncurry (!!)) $ zip s [0..] -> somma elementi diagonale principale
--- sum $ map (uncurry (!!)) $ zip (map reverse s) [0..] -> somma elementi diagonale secondaria
-isMagic s = 
-    (==1) $ -- check se lista di somme = 1
-    length $ 
-    nub $ 
-    concat [map sum s,  -- somma dei numeri delle righe
-            map sum $ transpose s,  -- somma dei numeri sulle colonne (ogni riga diventa colonna)
-            [sum $ map (uncurry (!!)) $ zip s [0..]],  -- somma elementi diagonale principale
-            [sum $ map (uncurry (!!)) $ zip (map reverse s) [0..]]]  -- somma elementi diagonale secondaria
-    
------------------------------------------------
-
--- la matrice in input deve essere separata in righe e poi per ogni elemento
--- lines -> divide le righe
--- words -> separara gli elementi
--- read -> converte in numeri gli elementi 
--- map (map read . words) . lines -> crea una lista di liste di numeri
--- show -> converte da int a string
-
+allMagic = concat [ take 4 $ iterate rot90 magic,
+                    take 4 $ iterate rot90 $ refl magic ]
+                    
 getIntList :: IO [Int]
 getIntList = fmap read getLine
 
-calcola = do
+computeDistance = do
     putStrLn "----------------------------------------------------------------"
-    putStrLn "Inserisci la lista di numeri della matrice 3x3:: "
+    putStrLn "Inserisci la lista di numeri della matrice 3x3 :"
+    putStrLn "es. [1,2,3,4,5,6,7,8,9]"
     inputList <- getIntList
-    let result = solve $ chop 3 inputList
-    print(result)
+    let minDistance = solve $ chop 3 inputList
+    putStrLn $ "Minima distanza dal magic square più vicina : " ++ show minDistance
+    continueGameOrNot
 
-main = calcola
+continueGameOrNot :: IO ()
+continueGameOrNot = do
+    putStrLn "----------------------------------------------------------------"
+    putStrLn "Vuoi calcolare un'altra distanza? [Y/N]: "
+    choice <- getLine
+    case choice of
+        "Y" -> computeDistance
+        "Yes" -> computeDistance
+        "y" -> computeDistance
+        _ -> return ()
+
+main = computeDistance
